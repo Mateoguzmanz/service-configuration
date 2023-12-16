5fad97e83f9ef3e7fbe9bd647a27c72af2045e88
mateguzm <mateoguzmanz@hotmail.com>
2023-12-15 19:48:33 -0500

    TODO

---
+++ src/main/java/com/example/ServiceConfig/configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.ServiceConfig.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+   /* @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build()
+        );*/
+
+    }
